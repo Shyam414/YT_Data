17beca9e88d6fddde549bce9792f1c6ec2cd8c11
Your Name <you@example.com>
2024-05-19 21:23:43 +0530

    hi

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import psycopg2
 
+# Read the CSV data into a DataFrame
+df = pd.read_csv('Data\\CleanData.csv')
 
-df=pd.read_csv('Data\\CleanData.csv')
+# Function to convert NaN to None
+def convert_nan_to_none(val):
+    return None if pd.isna(val) else val
 
+# Apply the conversion function to the DataFrame
+df = df.applymap(convert_nan_to_none)
+
+# Database connection parameters
 conn_params = {
     "host": "127.0.0.2",
     "port": "5432",
@@ -12,14 +20,13 @@ conn_params = {
     "database": "YT_Data"
 }
 
+# Establish the connection
 conn = psycopg2.connect(**conn_params)
-
 cur = conn.cursor()
 
-# Drop table yt if it exists
+# Drop the table if it exists
 cur.execute("DROP TABLE IF EXISTS yt")
 conn.commit()
-
 
 # Define the table creation query
 create_table_query = '''
@@ -31,7 +38,6 @@ CREATE TABLE IF NOT EXISTS yt (
     publishedAt TIMESTAMP,
     viewCount FLOAT,
     likeCount FLOAT,
-    favouriteCount FLOAT,
     commentCount FLOAT,
     duration VARCHAR,
     definition VARCHAR,
@@ -51,18 +57,17 @@ for index, row in df.iterrows():
     insert_query = """
     INSERT INTO yt (
         video_id, channelTitle, title, tags, publishedAt,
-        viewCount, likeCount, favouriteCount, commentCount, duration,
+        viewCount, likeCount, commentCount, duration,
         definition, caption, pushblishDayName, durationSecs, tagCount
-    )
-    VALUES (%s, %s, %s,  %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
     """
-    cur.execute(insert_query, tuple(row))
+    cur.execute(insert_query, (
+        row['video_id'], row['channelTitle'], row['title'], row['tags'], row['publishedAt'],
+        row['viewCount'], row['likeCount'], row['commentCount'], row['duration'],
+        row['definition'], row['caption'], row['pushblishDayName'], row['durationSecs'], row['tagCount']
+    ))
     conn.commit()
 
 # Close cursor and connection
 cur.close()
 conn.close()
-
-
-
-
